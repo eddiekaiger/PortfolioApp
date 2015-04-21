7acbf399822c4f456523929b9b8f2b7eb66177e7
Eddie Kaiger <eddiekaiger@gmail.com>
2015-04-21 02:42:55 -0700

    Finished WWDC page.

--- Code/ViewController/AboutMeWWDCViewController.swift
+++ Code/ViewController/AboutMeWWDCViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class AboutMeWWDCViewController: BaseChildViewController {
 
+    
+    @IBOutlet weak private var headerLabel: UILabel!
+    @IBOutlet weak private var wwdcLabel: UILabel!
+
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureHeaderLabel()
+        configureWWDCLabel()
     }
 
+    // MARK: - Configure
+    
+    private func configureHeaderLabel() {
+        self.headerLabel.text = "Oh, and I've always\nonly dreamed of attending"
+        self.headerLabel.textAlignment = .Center
+        self.headerLabel.textColor = self.defaultTextColor
+        self.headerLabel.numberOfLines = 0
+        self.headerLabel.font = UIFont.font(EKFontType.LightItalic, fontSize: 18)
+    }
+    
+    private func configureWWDCLabel() {
+        self.wwdcLabel.text = "WWDC"
+        self.wwdcLabel.textColor = self.defaultTextColor
+        self.wwdcLabel.textAlignment = .Center
+        self.wwdcLabel.font = UIFont.font(EKFontType.Light, fontSize: 60)
+    }
 
+    // MARK: - EKScrollingDelegate
+    
+    override func onScrollWithPageOnRight(offset: CGFloat) {
+        self.headerLabel.transform = CGAffineTransformMakeRotation(offset * CGFloat(M_PI_2))
+        self.wwdcLabel.transform = CGAffineTransformMakeRotation(-offset * CGFloat(M_PI_2))
+    }
+    
+    override func onScrollWithPageOnLeft(offset: CGFloat) {
+        
+    }
+    
 }
