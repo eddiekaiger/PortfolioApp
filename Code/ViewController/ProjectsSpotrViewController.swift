7116c584516b563c7d05bd3824fb440c05856afc
Eddie Kaiger <eddiekaiger@gmail.com>
2015-04-25 02:24:26 -0700

    Created Weeblog scene.

--- Code/ViewController/ProjectsSpotrViewController.swift
+++ Code/ViewController/ProjectsSpotrViewController.swift
@@ -10,24 +10,48 @@ import UIKit
 
 class ProjectsSpotrViewController: BaseChildViewController {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        
+        configureDescriptionLabel()
+        configureLogoImageView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.logoImageView.layer.cornerRadius = self.logoImageView.height / 2.0
     }
     
     // MARK: - Configure
     
+    func configureDescriptionLabel() {
+        self.descriptionLabel.text = "Spotr is an app that makes use of a notification center widget so that you never again forget where you left something. Spotr is still under development."
+        self.descriptionLabel.textColor = self.defaultTextColor
+        self.descriptionLabel.textAlignment = .Center
+        self.descriptionLabel.numberOfLines = 0
+        self.descriptionLabel.font = UIFont.font(EKFontType.Light, fontSize: 20)
+    }
     
+    func configureLogoImageView() {
+        self.logoImageView.layer.borderColor = UIColor.whiteColor().CGColor
+        self.logoImageView.layer.borderWidth = 4
+        self.logoImageView.layer.masksToBounds = true
+    }
     
     // EKScrollingDelegate
     
     override func onScrollWithPageOnRight(offset: CGFloat) {
-        
+        self.logoImageView.transform = CGAffineTransformMakeRotation(-offset * CGFloat(M_PI))
     }
     
     override func onScrollWithPageOnLeft(offset: CGFloat) {
-        
+        self.logoImageView.transform = CGAffineTransformTranslate(CGAffineTransformMakeRotation(offset * CGFloat(M_PI)),
+            offset * self.view.width, -offset * self.view.height)
+        self.descriptionLabel.transform = CGAffineTransformMakeTranslation(self.view.width * offset, self.view.height * -offset)
     }
 }
--- Code/ViewController/ProjectsWeeblogViewController.swift
+++ Code/ViewController/ProjectsWeeblogViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ProjectsWeeblogViewController: BaseChildViewController {
 
+    
+    @IBOutlet weak var screenshotImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        
+        configureScreenshot()
+        configureDescriptionLabel()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.screenshotImageView.layer.shadowPath = UIBezierPath(rect: self.screenshotImageView.bounds).CGPath
     }
     
     // MARK: - Configure
     
+    func configureScreenshot() {
+        self.screenshotImageView.layer.shadowColor = UIColor.blackColor().CGColor
+        self.screenshotImageView.layer.shadowOpacity = 0.8
+        self.screenshotImageView.layer.shadowOffset = CGSizeMake(2, 2)
+    }
     
+    func configureDescriptionLabel() {
+        self.descriptionLabel.text = "Weeblog was an internship interview project for Weebly that I made over the course of a few weeks. The app is a fully-featured local blog built on Core Data, containing photo, video, and rich text support. (Oh, and I got the internship.)"
+        self.descriptionLabel.textColor = self.defaultTextColor
+        self.descriptionLabel.textAlignment = .Center
+        self.descriptionLabel.numberOfLines = 0
+        self.descriptionLabel.font = UIFont.font(EKFontType.Light, fontSize: 16)
+    }
     
     // EKScrollingDelegate
     
